537497559baae4ec5747103258b3cdcc67dddc3a
thur <arthur_ferrari@estudante.sesisenai.org.br>
2025-09-17 13:47:51 -0300

    Commit 4 - Adicionando os Códigos

---
+++ dbescola/src/main/java/psii/senai/dbescola/controller/AlunoController.java
@@ -0,0 +1,33 @@
+package psii.senai.dbescola.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import psii.senai.dbescola.model.Aluno;
+import psii.senai.dbescola.repository.AlunoRepository;
+
+@Controller
+public class AlunoController {
+
+    private final AlunoRepository repository;
+
+    public AlunoController(AlunoRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/alunos")
+    public String index(Model model) {
+        model.addAttribute("alunos", repository.findAll());
+        model.addAttribute("aluno", new Aluno());
+        return "alunos";
+    }
+
+    @PostMapping("/alunos/salvar")
+    public String salvar(@ModelAttribute Aluno aluno) {
+        repository.save(aluno);
+        return "redirect:/alunos";
+    }
+}
---
+++ dbescola/src/main/java/psii/senai/dbescola/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package psii.senai.dbescola.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import psii.senai.dbescola.model.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
+
